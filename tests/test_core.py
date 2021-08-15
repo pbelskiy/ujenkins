255a49ce9968c21f593a87b6c91d3e917cf11aba
Petr Belskiy <petr.belskiy@gmail.com>
2021-08-16 00:28:24 +0300

    add async retry test

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,8 +1,10 @@
 from http import HTTPStatus
 
+import aiohttp
+import pytest
 import responses
 
-from ujenkins import JenkinsClient
+from ujenkins import AsyncJenkinsClient, JenkinsClient, JenkinsError
 
 
 @responses.activate
@@ -24,3 +26,65 @@ def test_sync_client_retry():
     )
 
     assert client.session.adapters['http://'].max_retries.status_forcelist == statuses
+
+
+@pytest.mark.asyncio
+async def test_async_client_retry(aiohttp_mock):
+    client = AsyncJenkinsClient(
+        'http://server',
+        'login',
+        'password',
+        retry=dict(
+            total=10,
+            statuses=[HTTPStatus.INTERNAL_SERVER_ERROR],
+        )
+    )
+    client.crumb = False
+
+    aiohttp_mock.get(
+        'http://server/',
+        content_type='text/plain',
+        body='Server error',
+        status=HTTPStatus.INTERNAL_SERVER_ERROR,
+    )
+
+    aiohttp_mock.get(
+        'http://server/',
+        content_type='text/plain',
+        headers={'X-Jenkins': '2.0.129'},
+        status=HTTPStatus.OK,
+    )
+
+    version = await client.system.get_version()
+    assert version.major == 2
+    assert version.minor == 0
+    assert version.patch == 129
+    await client.close()
+
+
+@pytest.mark.asyncio
+async def test_async_client_retry_exception(aiohttp_mock):
+    client = AsyncJenkinsClient(
+        'http://server',
+        'login',
+        'password',
+        retry=dict(
+            total=2,
+            statuses=[HTTPStatus.INTERNAL_SERVER_ERROR],
+        )
+    )
+
+    aiohttp_mock.get(
+        'http://server/rest/version',
+        exception=aiohttp.ClientError()
+    )
+
+    aiohttp_mock.get(
+        'http://server/rest/version',
+        exception=aiohttp.ClientError()
+    )
+
+    with pytest.raises(JenkinsError):
+        await client.system.get_version()
+
+    await client.close()
