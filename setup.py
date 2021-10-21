ca7feff1595c2c058087c3a6cd8fa17d1d868d2d
Petr Belskiy <petr.belskiy@gmail.com>
2021-10-21 21:15:03 +0300

    remove extra metadata

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ setup_args = dict(
     packages=find_packages(),
     package_data={'ujenkins': ['*']},
     author='Petr Belskiy',
-    author_email='petr.belskiy@gmail.com',
     keywords=['jenkins'],
     url='https://github.com/pbelskiy/ujenkins',
     download_url='https://pypi.org/project/ujenkins'
