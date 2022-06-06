f0acf9b4c21c32c73e3577f77f3155b13f83331f
Petr Belskiy <petr.belskiy@gmail.com>
2022-06-07 00:41:40 +0300

    bump version to 0.3.0 (nodes)

--- ujenkins/__init__.py
+++ ujenkins/__init__.py
@@ -2,7 +2,7 @@ from ujenkins.adapters.aio import AsyncJenkinsClient
 from ujenkins.adapters.sync import JenkinsClient
 from ujenkins.exceptions import JenkinsError, JenkinsNotFoundError
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 __all__ = (
     # adapters
