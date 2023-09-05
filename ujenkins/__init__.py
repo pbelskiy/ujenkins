255c076623a3397fbfc9adc3accb82185a82a6ba
Petr Belskiy <petr.belskiy@gmail.com>
2023-09-05 16:54:07 +0300

    update version to 0.10.2

--- ujenkins/__init__.py
+++ ujenkins/__init__.py
@@ -2,7 +2,7 @@ from ujenkins.adapters.aio import AsyncJenkinsClient
 from ujenkins.adapters.sync import JenkinsClient
 from ujenkins.exceptions import JenkinsError, JenkinsNotFoundError
 
-__version__ = '0.10.1'
+__version__ = '0.10.2'
 
 __all__ = (
     # adapters
