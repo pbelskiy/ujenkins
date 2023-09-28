a361905390d27383fa46453847f9979e8df60646
Gene Myslinsky <47491689+GeneMyslinsky@users.noreply.github.com>
2023-09-28 09:20:29 -0400

    builds.get() new arg: fields (#18)
    * Builds.get new args (fields, start, end)
    
    * resolve all issues except slice and tests
    
    * add tests to satisfy code coverage
    
    * resolve pr issues
    
    test: test_get_selective - url verification
    fixup docstring format
    some naming changes
    
    space between lines in docstring
    
    add parameterized tests
    
    add expected failure to tests
    
    * make url relative in tests, fix docs

---
+++ tests/test_builds_response.py
@@ -0,0 +1,258 @@
+import json
+
+# Sample usage
+builds_data = [
+    {
+        '_class': 'org.jenkinsci.plugins.workflow.job.WorkflowRun',
+        'actions': [
+            {'_class': 'hudson.model.CauseAction'},
+            {'_class': 'jenkins.metrics.impl.TimeInQueueAction'},
+            {'_class': 'org.jenkinsci.plugins.workflow.libs.LibrariesAction'},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {'_class': 'org.jenkinsci.plugins.displayurlapi.actions.RunDisplayAction'},
+            {},
+            {'_class': 'org.jenkinsci.plugins.workflow.job.views.FlowGraphAction'},
+            {},
+            {},
+            {},
+            {}
+        ],
+        'artifacts': [],
+        'building': False,
+        'description': None,
+        'displayName': '#5',
+        'duration': 110338,
+        'estimatedDuration': 55,
+        'executor': None,
+        'fingerprint': [],
+        'fullDisplayName': '💻 dev » testgene #5',
+        'id': '5',
+        'keepLog': False,
+        'number': 5,
+        'queueId': 12216,
+        'result': 'SUCCESS',
+        'timestamp': 1695091214082,
+        'url': 'https://localhost:8080/job/dev/job/testgene/5/',
+        'changeSets': [],
+        'culprits': [],
+        'inProgress': False,
+        'nextBuild': {},
+        'previousBuild': {}
+    },
+    {
+        '_class': 'org.jenkinsci.plugins.workflow.job.WorkflowRun',
+        'actions': [
+            {'_class': 'hudson.model.CauseAction'},
+            {'_class': 'jenkins.metrics.impl.TimeInQueueAction'},
+            {'_class': 'org.jenkinsci.plugins.workflow.libs.LibrariesAction'},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {'_class': 'org.jenkinsci.plugins.displayurlapi.actions.RunDisplayAction'},
+            {},
+            {'_class': 'org.jenkinsci.plugins.workflow.job.views.FlowGraphAction'},
+            {},
+            {},
+            {},
+            {}
+        ],
+        'artifacts': [],
+        'building': False,
+        'description': None,
+        'displayName': '#4',
+        'duration': 110411,
+        'estimatedDuration': 55,
+        'executor': None,
+        'fingerprint': [],
+        'fullDisplayName': '💻 dev » testgene #4',
+        'id': '4',
+        'keepLog': False,
+        'number': 4,
+        'queueId': 12214,
+        'result': 'SUCCESS',
+        'timestamp': 1695090700131,
+        'url': 'https://localhost:8080/job/dev/job/testgene/4/',
+        'changeSets': [],
+        'culprits': [],
+        'inProgress': False,
+        'nextBuild': {},
+        'previousBuild': {}
+    },
+    {
+        '_class': 'org.jenkinsci.plugins.workflow.job.WorkflowRun',
+        'actions': [
+            {'_class': 'hudson.model.CauseAction'},
+            {'_class': 'jenkins.metrics.impl.TimeInQueueAction'},
+            {'_class': 'org.jenkinsci.plugins.workflow.libs.LibrariesAction'},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {'_class': 'org.jenkinsci.plugins.displayurlapi.actions.RunDisplayAction'},
+            {},
+            {'_class': 'org.jenkinsci.plugins.workflow.job.views.FlowGraphAction'},
+            {},
+            {},
+            {},
+            {}
+        ],
+        'artifacts': [],
+        'building': False,
+        'description': None,
+        'displayName': '#3',
+        'duration': 110375,
+        'estimatedDuration': 55,
+        'executor': None,
+        'fingerprint': [],
+        'fullDisplayName': '💻 dev » testgene #3',
+        'id': '3',
+        'keepLog': False,
+        'number': 3,
+        'queueId': 12212,
+        'result': 'SUCCESS',
+        'timestamp': 1695087505819,
+        'url': 'https://localhost:8080/job/dev/job/testgene/3/',
+        'changeSets': [],
+        'culprits': [],
+        'inProgress': False,
+        'nextBuild': {},
+        'previousBuild': {}
+    },
+    {
+        '_class': 'org.jenkinsci.plugins.workflow.job.WorkflowRun',
+        'actions': [
+            {'_class': 'hudson.model.CauseAction'},
+            {'_class': 'jenkins.metrics.impl.TimeInQueueAction'},
+            {'_class': 'org.jenkinsci.plugins.workflow.libs.LibrariesAction'},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {'_class': 'org.jenkinsci.plugins.displayurlapi.actions.RunDisplayAction'},
+            {},
+            {'_class': 'org.jenkinsci.plugins.workflow.job.views.FlowGraphAction'},
+            {},
+            {},
+            {},
+            {}
+        ],
+        'artifacts': [],
+        'building': False,
+        'description': None,
+        'displayName': '#2',
+        'duration': 110381,
+        'estimatedDuration': 55,
+        'executor': None,
+        'fingerprint': [],
+        'fullDisplayName': '💻 dev » testgene #2',
+        'id': '2',
+        'keepLog': False,
+        'number': 2,
+        'queueId': 12210,
+        'result': 'SUCCESS',
+        'timestamp': 1695087480148,
+        'url': 'https://localhost:8080/job/dev/job/testgene/2/',
+        'changeSets': [],
+        'culprits': [],
+        'inProgress': False,
+        'nextBuild': {},
+        'previousBuild': {}
+    },
+    {
+        '_class': 'org.jenkinsci.plugins.workflow.job.WorkflowRun',
+        'actions': [
+            {'_class': 'hudson.model.CauseAction'},
+            {'_class': 'jenkins.metrics.impl.TimeInQueueAction'},
+            {'_class': 'org.jenkinsci.plugins.workflow.libs.LibrariesAction'},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {'_class': 'org.jenkinsci.plugins.displayurlapi.actions.RunDisplayAction'},
+            {},
+            {'_class': 'org.jenkinsci.plugins.workflow.job.views.FlowGraphAction'},
+            {},
+            {},
+            {},
+            {}
+        ],
+        'artifacts': [],
+        'building': False,
+        'description': None,
+        'displayName': '#1',
+        'duration': 110528,
+        'estimatedDuration': 55,
+        'executor': None,
+        'fingerprint': [],
+        'fullDisplayName': '💻 dev » testgene #1',
+        'id': '1',
+        'keepLog': False,
+        'number': 1,
+        'queueId': 12208,
+        'result': 'SUCCESS',
+        'timestamp': 1695087098240,
+        'url': 'https://localhost:8080/job/dev/job/testgene/1/',
+        'changeSets': [],
+        'culprits': [],
+        'inProgress': False,
+        'nextBuild': {},
+        'previousBuild': None
+    }
+]
+
+
+def filter_builds_response(fields=None, start=None, end=None):
+    """
+    Dynamically filters the Jenkins data.
+
+    Args:
+    - data (list of dict):
+        The list containing Jenkins build data.
+
+    - fields (list of str, optional):
+        The fields you want to retain. If None, or contains '*', all fields are retained
+
+
+    Returns:
+    - JSON (str):
+         Filtered Jenkins build data in JSON format.
+    """
+
+    # Filter by range if start and end are provided
+    data = builds_data
+    data = data[start-1:end]
+
+    # Filter by fields if provided and * is not in fields
+    if fields and '*' not in fields:
+        data = [{k: v for k, v in item.items() if k in fields or k == '_class'} for item in data]
+
+    return json.dumps(
+            {
+                '_class': 'hudson.model.FreeStyleProject',
+                'allBuilds': data
+            }
+        )
--- ujenkins/endpoints/builds.py
+++ ujenkins/endpoints/builds.py
@@ -21,10 +21,42 @@ class Builds:
     def __init__(self, jenkins) -> None:
         self.jenkins = jenkins
 
-    def get(self, name: str) -> List[dict]:
+    def get(self,
+            name: str,
+            *,
+            fields: Optional[List[str]] = None,
+            start: Optional[int] = None,
+            end: Optional[int] = None
+            ) -> List[dict]:
         """
         Get list of builds for specified job.
 
+        Available fields in jenkins:
+            - _class
+            - actions
+            - artifacts
+            - building
+            - description
+            - displayName
+            - duration
+            - estimatedDuration
+            - executor
+            - fingerprint
+            - fullDisplayName
+            - id
+            - keepLog
+            - number
+            - queueId
+            - result
+            - timestamp
+            - url
+            - changeSets
+            - culprits
+            - inProgress
+            - nextBuild
+            - previousBuild
+
+        Use * inside the list to get all fields.
         Example:
 
         .. code-block:: python
@@ -38,17 +70,40 @@ class Builds:
             name (str):
                 Job name or path (if in folder).
 
+            fields (Optional[List]):
+                List of fields to return.
+                Possible values are mentioned in the available fields.
+                If empty, default fields will be returned.
+
+            start (Optional[int]):
+                The start index of the builds to retrieve.
+                Used with the 'end' parameter to specify a range.
+                Defaults to None.
+
+            end (Optional[int]):
+                The end index of the builds to retrieve.
+                Used with the 'start' parameter to specify a range.
+                Defaults to None.
+
         Returns:
-            List: list of build for specified job.
+            List[dict]: list of builds for specified job.
         """
+
         def callback(response) -> List[dict]:
             return json.loads(response.text)['allBuilds']
 
+        pagination = ''
+        if end is not None or start is not None:
+            start_str = start if start is not None else ''
+            end_str = end if end is not None else ''
+            pagination = f'{{{start_str},{end_str}}}'
+
         folder_name, job_name = self.jenkins._get_folder_and_job_name(name)
 
+        fields_str = ','.join(fields) if fields else 'number,url'
         return self.jenkins._request(
             'GET',
-            f'/{folder_name}/job/{job_name}/api/json?tree=allBuilds[number,url]',
+            f'/{folder_name}/job/{job_name}/api/json?tree=allBuilds[{fields_str}]{pagination}',
             _callback=callback,
         )
 
