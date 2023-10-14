aa36b80b3be9d8dfd2a11df48edffe0776f62f89
Petr Belskiy <petr.belskiy@gmail.com>
2023-10-14 19:57:10 +0300

    add Python 3.12

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup_args = dict(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
     license='MIT',
     packages=find_packages(),
