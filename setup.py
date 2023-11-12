0dba56c730da0ab9c3050c2deeff606b366b8653
Petr Belskiy <petr.belskiy@gmail.com>
2023-11-12 15:25:16 +0300

    Revert "add Python 3.12"
    This reverts commit aa36b80b3be9d8dfd2a11df48edffe0776f62f89.

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup_args = dict(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
     ],
     license='MIT',
     packages=find_packages(),
