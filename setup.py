cef45914c05523861d742ba14d4077ff4cd24adc
Petr Belskiy <petr.belskiy@gmail.com>
2026-02-08 02:26:58 +0300

    update python versions matrix

--- setup.py
+++ setup.py
@@ -26,10 +26,12 @@ setup_args = dict(
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+        'Programming Language :: Python :: 3.15',
     ],
     license='MIT',
     packages=find_packages(),
@@ -48,6 +50,6 @@ install_requires = [
 
 setup(
     install_requires=install_requires,
-    python_requires='>=3.9',
+    python_requires='>=3.10',
     **setup_args
 )
