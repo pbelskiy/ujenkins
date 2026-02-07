42dc9c863ddc3c2b57314c94d8fe7b3a1483a571
Petr Belskiy <petr.belskiy@gmail.com>
2026-02-08 02:28:05 +0300

    bump release version

--- ujenkins/__init__.py
+++ ujenkins/__init__.py
@@ -2,7 +2,7 @@ from ujenkins.adapters.aio import AsyncJenkinsClient
 from ujenkins.adapters.sync import JenkinsClient
 from ujenkins.exceptions import JenkinsError, JenkinsNotFoundError
 
-__version__ = '0.11.1'
+__version__ = '0.12'
 
 __all__ = (
     # adapters
